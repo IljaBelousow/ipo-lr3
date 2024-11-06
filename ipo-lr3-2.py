47ad33047aadcaaeb1bfe7131c4a1c2ef7916846
qwe <qwe@gmail.com>
2024-11-06 13:40:58 +0300

    First commit

---
+++ ipo-lr3-2.py
@@ -0,0 +1,9 @@
+a = int(input("введимт число odin "))
+b = int(input("введимт число dva "))
+
+if a > b:
+    print("pervoe chislo bolshe")
+if b > a:
+    print("vtoroe chislo bolshe")
+if a == b:
+    print("chisla ravnis")
