b3e552d4363d1637eff0189b7568b8fee9b063a3
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-11-22 15:13:35 +0300

    Delete ipo-lr3-2.py

--- ipo-lr3-2.py
+++
@@ -1,9 +0,0 @@
-a = int(input("введимт число odin "))
-b = int(input("введимт число dva "))
-
-if a > b:
-    print("pervoe chislo bolshe")
-if b > a:
-    print("vtoroe chislo bolshe")
-if a == b:
-    print("chisla ravnis")
